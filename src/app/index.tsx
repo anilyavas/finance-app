7ac0a7dd477e5a83ca228224140be9e8e1d5b83e
anilyavas <anil.yavas99@gmail.com>
2024-05-01 19:05:55 +0300

    expo router fixed setup version

---
+++ src/app/index.tsx
@@ -0,0 +1,18 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>index</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
